d0797b87d4216ddb575f79dcc44807f253a9314c
Abaranasia <rankirlian@gmail.com>
2025-10-24 22:05:47 +0200

    style: add basic css and import resources css

--- laravel-login/resources/views/components/layout.blade.php
+++ laravel-login/resources/views/components/layout.blade.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'App' }}</title>
+    @vite('resources/css/app.css')
     <style>
         body {
-            background-color: grey;
+            background-color: gray;
         }
     </style>
 </head>

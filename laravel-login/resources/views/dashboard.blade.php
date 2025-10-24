bbc50a8905c697f516d912a78fe9e01932f17d6e
Abaranasia <rankirlian@gmail.com>
2025-10-24 22:06:18 +0200

    style: add layout component to dashboard view

--- laravel-login/resources/views/dashboard.blade.php
+++ laravel-login/resources/views/dashboard.blade.php
@@ -1,3 +1,5 @@
-<h1>
-    Welcome {{ Auth::user()->name }} to the private dashboard.
-</h1>
+<x-layout>
+    <h1>
+        Welcome {{ Auth::user()->name }} to the private dashboard.
+    </h1>
+</x-layout>

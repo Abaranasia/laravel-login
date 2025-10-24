478287ecf4b6bc8e126a38252b94cd9528ed7a80
Abaranasia <rankirlian@gmail.com>
2025-10-24 22:07:39 +0200

    chore: update router to connect login post to controler & add dashboard private view

--- laravel-login/routes/web.php
+++ laravel-login/routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('login', function() {
+    return view('login');
+})->name('login');
+
+Route::post('login', LoginController::class)->name('login.attempt');
+
+// Private views
+Route::get('dashboard', function () {
+    return view('dashboard');
 });
